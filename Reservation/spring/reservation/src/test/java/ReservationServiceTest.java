52d0e2f88e1df2f1465b6c639d4b1fcdf6ed0fb3
PKingTeak <119428177+PKingTeak@users.noreply.github.com>
2026-09-14 15:45:01 +0900

    JUnit + Mockito 테스트 적용 중복예약 시스템 테스트 완료

---
+++ Reservation/spring/reservation/src/test/java/ReservationServiceTest.java
@@ -0,0 +1,82 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.reservation.Facility.*;
+import com.example.reservation.Facility.DTO.ReservationRequest;
+import com.example.reservation.Facility.Exception.DuplicateDataException;
+import com.example.reservation.Facility.Repository.ReservationRepository;
+
+@ExtendWith (MockitoExtension.class)
+
+public class ReservationServiceTest {
+    
+    @Mock 
+    UserService tUserService;
+    
+    @Mock 
+    FacilityService tFacilityService;
+    @Mock 
+    ReservationRepository tRepository;
+    
+    
+    
+    @Mock 
+    User tUser;
+    @Mock 
+    Facility tFacility;
+    
+    @InjectMocks 
+    ReservationService tReservationService;
+    
+    
+    @DisplayName ("중복 예약 방지 테스트")
+    @Test 
+    void ReservationDuplicationTest()
+    {
+        //Given 테스트 상황
+        String email = "test@test.com";
+        long facilityId = 1;
+        LocalDate date = LocalDate.of(2026,9,20);
+
+        Reservation existingReservation= new Reservation(tUser,tFacility,date,LocalTime.of(10,0),LocalTime.of(12,0)); 
+
+
+        ReservationRequest request = new ReservationRequest(); //들어오는 예약 중복으로 예약 해야함
+        
+        request.setFacilityId(facilityId);
+        request.setStartTime(LocalTime.of(10,0));
+        
+        request.setEndTime(LocalTime.of(13,0));
+
+        request.setDate(date);
+
+
+        //When
+        when(tUserService.getUserByEmail(email)).thenReturn(tUser);
+        when(tFacilityService.geFacilityInfoById(facilityId)).thenReturn(tFacility);
+        when(tReservationService.getReservationsByFacilityAndDate(facilityId, date)).thenReturn(List.of(existingReservation));
+        
+
+        //When + Then 같이 사용해서 Return 반환 가능하다. 
+        assertThrows(DuplicateDataException.class,()->tReservationService.addReservation(request, email));
+
+
+        verify(tRepository, never()).save(any(Reservation.class));
+        
+    }
+
+
+}
